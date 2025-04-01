d8758a484d0460369968989da5ce9d4c074f037f
Mateus Sfeir <mateussfeir@hotmail.com>
2025-04-01 11:14:28 -0400

    Week13

--- Week12/tick.py
+++ Week12/tick.py
@@ -1,10 +1,10 @@
 # Tick class for aggregation
 class Tick:
     def __init__(self):
-        print("Aggrigation: Tick is Created.")
+        print("Aggregation: Tick is Created.")
 
     def consume_blood(self):
         print("Tick is consuming blood..")
 
     def __del__(self):
-        print("Aggrigation: Tick is destroyed,")
+        print("Aggregation: Tick is destroyed,")
